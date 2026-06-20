5e8be19848be0c29f154de9c717e1527c2d1cf23
anhlephuong <phuonganhhd6789@gmail.com>
2026-06-20 23:03:37 +0700

    Add files via upload

---
+++ quan-ly-rap-chieu-phim/src/movies/dto/create-movie.dto.ts
@@ -0,0 +1,36 @@
+import { IsString, IsNotEmpty, IsOptional, IsInt, IsPositive, IsDateString } from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Tên phim không được để trống' })
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  genre?: string;
+
+  @IsInt()
+  @IsPositive({ message: 'Thời lượng phải lớn hơn 0' })
+  @IsOptional()
+  duration?: number;
+
+  @IsString()
+  @IsOptional()
+  director?: string;
+
+  @IsString()
+  @IsOptional()
+  actors?: string;
+
+  @IsDateString({}, { message: 'Ngày phát hành không đúng định dạng (YYYY-MM-DD)' })
+  @IsOptional()
+  release_date?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  poster?: string;
+}

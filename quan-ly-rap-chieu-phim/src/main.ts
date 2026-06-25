bec41a8ea66787916cbb101074b89dd963f8aac2
anhlephuong <phuonganhhd6789@gmail.com>
2026-06-25 08:18:16 +0700

    cai dat cookies va session

--- quan-ly-rap-chieu-phim/src/main.ts
+++ quan-ly-rap-chieu-phim/src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
- 
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // Bắt buộc để DTO validate hoạt động
+
+  app.use(cookieParser());
+
+  app.use(
+    session({
+      secret: 'my-secret-key', // Thay bằng giá trị bí mật thực tế (env variable)
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 3600 * 1000, // 1 giờ
+        sameSite: 'strict',
+      },
+    }),
+  );
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

fd271a039bf0d276594f6f6f2e655af85366b72b
anhlephuong <phuonganhhd6789@gmail.com>
2026-07-24 22:47:59 +0700

    Bật CORS

--- my-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Google logo', () => {
-  render(<App />);
-  const logoElement = screen.getByLabelText(/google/i);
-  expect(logoElement).toBeInTheDocument();
-});
--- quan-ly-rap-chieu-phim/src/main.ts
+++ quan-ly-rap-chieu-phim/src/main.ts
@@ -7,6 +7,11 @@ import session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3001',
+    credentials: true,
+  });
+
   app.use(cookieParser());
 
   app.use(
